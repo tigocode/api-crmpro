da3a71883d460f87ceff50f8821670b541c76912
tigocode <tiago.dantas16@live.com>
2025-01-19 21:58:25 -0300

    :sparkles: feat: inclusao do arquivo routes.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/* app.use(routes); */
 
 app.get('/users', async (req, res) => {
   res.json({mensagem: 'success!'});
 });
 
-app.listen(3000, () => {
+
+app.listen(process.env.PORT || 3000, () => {
   let date = new Date();
-  console.log(`Servidor rodando na PORT 3000 e em execução desde:  ${date.toLocaleString()}`);
+  console.log(`Servidor rodando na PORT 3000 e em execução desde: ${date.toLocaleString()}`);
 });
