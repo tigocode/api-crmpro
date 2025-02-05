0e576e906d57f27704fc0605305b0efc90c538c0
tigocode <tiago.dantas16@live.com>
2025-02-04 22:22:47 -0300

    :sparkles: feat: Inclusao de novas config no arquivo knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,30 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: 'src/connection/migrations',
-    }
+    },
+    seeds: {
+      directory: 'src/connection/seeds', 
+    },
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10, 
+    },
+    migrations: {
+      directory: 'src/connection/migrations',
+    },
+    seeds: {
+      directory: 'src/connection/seeds',
+    },
   }
-}
+};
