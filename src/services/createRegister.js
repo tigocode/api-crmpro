ce679dfbbba76f69bb78736678bcd3f7a573b236
tigocode <tiago.dantas16@live.com>
2025-04-24 23:36:34 -0300

    :sparkles: feat: Adiciona lógica de verificação de recompra e atualiza função de criação de registro

--- src/services/createRegister.js
+++ src/services/createRegister.js
@@ -1,11 +1,14 @@
 const connection = require('../connection/connection');
 
-const createRegister = async (produto, categoria, quantidade, data_recompra, id_client, id_user) => {
+const createRegister = async (produto, categoria, quantidade, data_recompra, is_repurachase, is_alert_set, alert_count, id_client, id_user) => {
   const id = await connection('register').insert({
     produto,
     categoria,
     quantidade,
     data_recompra,
+    is_repurachase,
+    is_alert_set,
+    alert_count,
     id_client,
     id_user
   });
